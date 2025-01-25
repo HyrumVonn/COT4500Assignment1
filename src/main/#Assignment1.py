98d1da20536e2465b4e6e59f711db62542f9b5d6
HyrumVonn <uberhimer@gmail.com>
2025-01-25 16:23:24 -0500

    Generalized float-from-Binary

--- src/main/#Assignment1.py
+++ src/main/#Assignment1.py
@@ -6,24 +6,24 @@ import numpy
 #given string:
 #0 10000000111 111010111001
 #0 11 52
-binary_data = '010000000111111010111001'
+def FloatFromBinString(binary_data):
+    for i in range(0, 64 - len(binary_data)):
+        binary_data = binary_data + "0"
 
-for i in range(0, 64 - len(binary_data)):
-    binary_data = binary_data + "0"
+    sign = pow(-1, int(binary_data[0]))
 
-sign = pow(-1, int(binary_data[0]))
+    exponent = 0
 
-exponent = 0
+    for i in range(1, 12):
+        exponent = exponent + int(binary_data[i]) * pow(2, 11-i)
 
-for i in range(1, 12):
-    exponent = exponent + int(binary_data[i]) * pow(2, 11-i)
+    mantissa = 0
 
-mantissa = 0
+    for i in range(12, 64):
+        mantissa = mantissa + int(binary_data[i]) * pow(0.5, i - 11)
 
-for i in range(12, 64):
-    mantissa = mantissa + int(binary_data[i]) * pow(0.5, i - 11)
+    return sign * pow(2, (exponent - 1023)) * (1 + mantissa)
 
-floatVal = sign * pow(2, (exponent - 1023)) * (1 + mantissa)
 
 #problem 1
 print(f"{floatVal:.5f}\n\n")
